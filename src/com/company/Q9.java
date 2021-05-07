103e2bb4307ca3deb973598c15d1ab753e8bd207
Mohamed Ragab <82184006+MohamedRagab2002@users.noreply.github.com>
2021-05-07 07:57:20 +0200

    Update Q9.java

--- src/com/company/Q9.java
+++ src/com/company/Q9.java
@@ -6,7 +6,7 @@ public class Q9 {
     public static void main(String[] args){
         int a = getRandom(), base = getRandom(), height = getRandom();
         System.out.println("Area = " + (base * height));
-        // Print perimeter
+       
         System.out.println("Perimeter = " + 2*(a + base));
     }
     static int getRandom(){
