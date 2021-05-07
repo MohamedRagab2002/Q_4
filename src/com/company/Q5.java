08ce131a5b5fff0ece40e2e638fc786c4de885ee
Mohamed Ragab <82184006+MohamedRagab2002@users.noreply.github.com>
2021-05-07 06:14:42 +0200

    Add files via upload

---
+++ src/com/company/Q5.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+public class Q5 {
+    public static void main(String[] args){
+    Scanner reader = new Scanner(System.in);
+   System.out.print("Enter x1 : ");
+   double x1 = reader.nextInt();
+   System.out.print("Enter y1: ");
+   double y1 = reader.nextInt();
+   System.out.print("Enter x2 ");
+   double x2 = reader.nextInt();
+   System.out.print("Enter y2: ");
+   double y2 = reader.nextInt();
+   System.out.println("Slope = " + ((y2 - y1)/(x2 - x1)));
+
+    }
+}
