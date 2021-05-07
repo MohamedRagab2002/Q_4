123cec49455cc7a2b5f87ea0fa93a289387bbfea
Mohamed Ragab <82184006+MohamedRagab2002@users.noreply.github.com>
2021-05-07 06:19:04 +0200

    Add files via upload

---
+++ src/com/company/Q1.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Random random = new Random();
+        String city;
+        String country;
+
+        System.out.println("Please enter your city: ");
+        city = reader.nextLine();
+        System.out.println("Please enter your country: ");
+        country = reader.nextLine();
+        String mo = country.substring(0,2) + ',' + city + random.nextInt(100);
+        System.out.println(mo);
+
+
+    }
+}
