a6c703efb5e4c7b4d5d63534a20a0c0e2a8acfa0
Mohamed Ragab <82184006+MohamedRagab2002@users.noreply.github.com>
2021-05-07 04:56:27 +0200

    Add files via upload

---
+++ src/com/company/Q4.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the number");
+
+        int power = reader.nextInt();
+
+        double result = Math.exp(power);
+        System.out.println("The Result is "   + result);
+    }
+}
